516369e12c4ce8f76007392a745369bdfd7ee8de
jaga07u <pradha02jagadish@gmail.com>
2025-03-03 19:27:33 +0530

    Use Client Error solved

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,5 @@
 // /* eslint-disable react-hooks/exhaustive-deps */
-// "use client"
+ "use client"
 // import React from 'react'
 // import { useState,useEffect } from 'react';
 // import { useRouter } from 'next/navigation'
